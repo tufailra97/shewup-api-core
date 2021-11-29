dbf8e9cb24c13465a48813c4ac92749f99d65751
tufailra97 <tufailra97@gmail.com>
2021-11-29 18:39:26 +0000

    chore: replaced config service with app config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,20 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './api/auth/auth.module';
+import { CategoriesModule } from './api/categories/categories.module';
+import { OrdersModule } from './api/orders/orders.module';
+import { ProductsModule } from './api/products/products.module';
+import { TagsModule } from './api/tags/tags.module';
 import { UsersModule } from './api/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppLoggerMiddleware } from './shared/middlewares/app.logger.middlware';
 import { AppConfigService } from './shared/services/app-configs/app.config.service';
-import { OrdersModule } from './api/orders/orders.module';
-import { ProductsModule } from './api/products/products.module';
-import { TagsModule } from './api/tags/tags.module';
-import { CategoriesModule } from './api/categories/categories.module';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { AppConfigModule } from './shared/services/app-configs/app.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    }),
-    ThrottlerModule.forRootAsync({
-      imports: [AppConfigModule],
-      inject: [AppConfigService],
-      useFactory: (appConfigService: AppConfigService) => {
-        return {
-          ttl: appConfigService.configs.APP_THROTTLE_TTL,
-          limit: appConfigService.configs.APP_THROTTLE_LIMIT
-        };
-      }
     }),
     AuthModule,
     UsersModule,
